aefee2fa51372640894516150acc926c2b06ab65
mena <mena.rizkalla1@gmail.com>
2025-02-08 01:44:51 +0200

    feat: Configure routing for auth endpoints

--- src/main/kotlin/plugins/Routing.kt
+++ src/main/kotlin/plugins/Routing.kt
@@ -1,13 +1,27 @@
 package com.jwtdemo.plugins
 
+import com.jwtdemo.authenticate
+import com.jwtdemo.data.user.UserDataSource
+import com.jwtdemo.getSecretInfo
+import com.jwtdemo.security.hashing.HashingService
+import com.jwtdemo.security.token.TokenConfig
+import com.jwtdemo.security.token.TokenService
+import com.jwtdemo.signIn
+import com.jwtdemo.signUp
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Application.configureRouting() {
+fun Application.configureRouting(
+    userDataSource: UserDataSource,
+    hashingService: HashingService,
+    tokenService: TokenService,
+    tokenConfig: TokenConfig
+) {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        signIn(userDataSource,hashingService,tokenService,tokenConfig)
+        signUp(hashingService,userDataSource)
+        authenticate()
+        getSecretInfo()
     }
 }
